740ea627c5fa3d1b51ce217374e5285eceede238
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2024-07-22 14:41:22 +0700

    find a way to add class attribute from parent to child

--- src/app/forms/label/label.component.ts
+++ src/app/forms/label/label.component.ts
@@ -10,5 +10,5 @@ import { Component, Input } from '@angular/core';
 export class LabelComponent {
   @Input() for = '';
   @Input() text = '';
-
+  @Input() className = '';
 }
--- src/app/seller-form/seller-form.component.ts
+++ src/app/seller-form/seller-form.component.ts
@@ -4,20 +4,21 @@ import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 import { LabelComponent } from '../forms/label/label.component';
 import { InputComponent } from '../forms/input/input.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-seller-form',
   standalone: true,
-  imports: [ReactiveFormsModule, LabelComponent, InputComponent],
+  imports: [ReactiveFormsModule, LabelComponent, InputComponent, ButtonComponent],
   templateUrl: './seller-form.component.html',
   styleUrl: './seller-form.component.scss'
 })
 export class SellerFormComponent {
   sellerForm = new FormGroup({
-    name: new FormControl(''),
-    password: new FormControl(''),
-    rePassword: new FormControl(''),
-    email: new FormControl(''),
+    name: new FormControl('ilham'),
+    password: new FormControl('lodash'),
+    rePassword: new FormControl('lodash'),
+    email: new FormControl('ilham@lodash.com'),
     image: new FormControl<File | null>(null),
   });
 
@@ -29,5 +30,15 @@ export class SellerFormComponent {
         console.log(element.value)
       }
     }
+  }
+
+  submitForm() {
+    const {name, email, password} = this.sellerForm.controls;
+    const data = new FormData();
+    data.append('name', name.value !== null ? name.value : '')
+    data.append('email', email.value !== null ? email.value : '')
+    data.append('password', password.value !== null ? password.value : '')
+
+
   }
 }
