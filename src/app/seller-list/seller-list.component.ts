25688623668def65b22faff95b9ec07983bd46f7
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2024-07-28 13:54:21 +0700

    component, feature: seller-list. delete single item

--- src/app/seller-list/seller-list.component.ts
+++ src/app/seller-list/seller-list.component.ts
@@ -11,10 +11,11 @@ import { HttpClient } from '@angular/common/http';
 export class SellerListComponent {
 
   sellers: any;
+  deleteLoading: boolean = false;
+
   constructor(private http: HttpClient) {
     http.get('http://localhost:3000/sellers').subscribe({
       next: (response) => {
-        console.log('response', response)
         this.sellers = response
       },
       error: (error) => {
@@ -24,13 +25,34 @@ export class SellerListComponent {
   }
 
   deleteAll() {
+    this.deleteLoading = true;
+
     this.http.delete('http://localhost:3000/sellers/truncate').subscribe({
       next: (response) => {
         console.log('deleteAll() response', response)
         this.sellers = [];
+        this.deleteLoading = false;
       },
       error: (error) => {
         console.log('deleteAll() error', error)
+        this.deleteLoading = false;
+      }
+    });
+  }
+  
+  deleteSingle(id: number) {
+    console.log('delete', id)
+    this.deleteLoading = true;
+
+    this.http.delete(`http://localhost:3000/sellers/${id}`).subscribe({
+      next: (response: any) => {
+        console.log('deleteSingle() response', response)
+        this.deleteLoading = false;
+        this.sellers = response.sellers;
+      },
+      error: (error) => {
+        console.log('deleteSingle() error', error)
+        this.deleteLoading = false;
       }
     });
   }
