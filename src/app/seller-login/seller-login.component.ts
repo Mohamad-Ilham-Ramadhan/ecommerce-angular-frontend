69d670a27d50c15dde50d09dbbdbd20495b540ed
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2024-08-23 10:27:11 +0700

    component: seller-login, add alert

--- src/app/seller-login/seller-login.component.ts
+++ src/app/seller-login/seller-login.component.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
+import { AlertComponent, AlertVariant} from '../alert/alert.component';
 import { LabelComponent } from '../forms/label/label.component';
 import { InputComponent } from '../forms/input/input.component';
 
 @Component({
   selector: 'app-seller-login',
   standalone: true,
-  imports: [LabelComponent, InputComponent, FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, LabelComponent, InputComponent, AlertComponent],
   templateUrl: './seller-login.component.html',
   styleUrl: './seller-login.component.scss'
 })
 export class SellerLoginComponent {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
+
+  loading = false;
+  alertText = '';
+  alertVariant: AlertVariant = 'primary';
+  showAlert = false;
+  closeAlert() {
+    this.showAlert = false;
+  }
+  
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(4)]),
@@ -24,6 +35,9 @@ export class SellerLoginComponent {
     console.log('submit', this.form.valid);
     if (!this.form.valid) return;
 
+    this.loading = true;
+
+    
     const {email, password} = this.form.controls;
     const data = new FormData();
     data.append('email', email.value !== null ? email.value : '')
@@ -31,14 +45,17 @@ export class SellerLoginComponent {
 
     this.http.post('http://localhost:3000/sellers/login', data).subscribe({
       next: (res: any) => {
+        this.loading = false;
         console.log('response', res)
+        this.showAlert = true;
         if (res.token) {
-          localStorage.setItem('loginToken', res.token);
-          // this.alertText = res.message;
-          // this.alertVariant = 'primary';
+          localStorage.setItem('sellerToken', res.token);
+          this.alertText = res.message;
+          this.alertVariant = 'primary';
+          // this.router.navigate([''])
         } else {
-          // this.alertText = 'Something went wrong';
-          // this.alertVariant = 'danger';
+          this.alertText = 'Something went wrong';
+          this.alertVariant = 'danger';
         }
         // this.showAlert = true;
         // this.isFormLoading = false;
@@ -46,10 +63,11 @@ export class SellerLoginComponent {
         // this.sellerForm.reset()
       },
       error: (e: HttpErrorResponse) => {
+        this.loading = false;
         console.log('THIS IS ERROR', e);
-        // this.alertText = e.error.message;
-        // this.alertVariant = 'danger';
-        // this.showAlert = true;
+        this.alertText = e.error.message;
+        this.alertVariant = 'danger';
+        this.showAlert = true;
         // this.isFormLoading = false;
       },
     })
