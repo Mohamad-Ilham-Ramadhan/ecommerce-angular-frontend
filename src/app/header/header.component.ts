e20184fcc5f0d586730a7d77ad50cb4e7f623ec2
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2024-08-30 10:37:31 +0700

    component: header, logged in nav.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { SellerService } from '../services/seller.service';
+import { LocalStorageService } from '../services/local-storage.service';
+import { EnvironmentService } from '../services/environment.service';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -9,5 +12,34 @@ import { SellerService } from '../services/seller.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(private http: HttpClient, private localStorageService: LocalStorageService, public env: EnvironmentService, public sellerService: SellerService) {
+  }
+
+  isSellerLoggedIn = false;
+
+  ngOnInit(): void {
+    console.log('seller', this.sellerService.seller)
+
+    if (this.localStorageService.getData('userToken')) {
+      console.log('user exist')
+
+    } else if (this.localStorageService.getData('sellerToken')) {
+      console.log('seller exist')
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${this.localStorageService.getData('sellerToken')}`)
+      this.http.get(this.env.apiUrl()+'/sellers/find-one', {headers}).subscribe({
+        next: (response: any) => {
+          this.sellerService.setSeller(response.seller);
+          this.isSellerLoggedIn = true;
+        },
+        error: (error) => {
+          console.log('get seller error', error)
+        }
+      })
+    }
+  }
+
+  logout() {
+    
+  }
 }
