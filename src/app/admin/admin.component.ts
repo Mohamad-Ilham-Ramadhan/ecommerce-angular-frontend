9aae9369e0873f0b962f130f163d8b053d517bdc
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2024-09-01 02:18:49 +0700

    component: home, show products

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet} from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { LocalStorageService } from '../services/local-storage.service';
+import { EnvironmentService } from '../services/environment.service';
 @Component({
   selector: 'app-admin',
   standalone: true,
@@ -9,5 +12,17 @@ import { RouterLink, RouterOutlet} from '@angular/router';
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+  constructor(private http: HttpClient, private localStorageService: LocalStorageService, private env: EnvironmentService) {}
 
+  truncateProducts() {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.localStorageService.getData('adminToken')}`);
+    this.http.delete(this.env.apiUrl()+'/products/truncate', { headers}).subscribe({
+      next: (response: any) => {
+        console.log('response', response);
+      },
+      error: (error: any) => {
+        console.log('error', error);
+      }
+    });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { LocalStorageService } from '../services/local-storage.service';
+import { EnvironmentService } from '../services/environment.service';
+import { IdrCurrencyService } from '../services/idr-currency.service';
+import { IdrPipe } from '../pipes/idr.pipe';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, IdrPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private http: HttpClient, private localStorageService: LocalStorageService, public env: EnvironmentService) {}
+
+  ngOnInit(): void {
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.localStorageService.getData('userToken')}`);
+    
+    this.http.get(this.env.apiUrl()+'/products', {headers}).subscribe({
+      next: (response: any) => {
+        console.log('get all products response', response);
+        this.products = response;
+      },
+      error: (error) => {
+        console.log('error', error)
+      }
+    });
+  }
+
+  products: any[] = [];
 
 }
