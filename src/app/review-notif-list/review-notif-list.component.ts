59e8f8a36e6b8f54df32760fd4d9e5828dabb363
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2024-09-04 12:15:07 +0700

    component: review-notif-list done

--- src/app/review-notif-list/review-notif-list.component.ts
+++ src/app/review-notif-list/review-notif-list.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 import { EnvironmentService } from '../services/environment.service';
 import { LocalStorageService } from '../services/local-storage.service';
 import { IdrPipe } from '../pipes/idr.pipe';
+import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-review-notif-list',
   standalone: true,
-  imports: [IdrPipe, DatePipe],
+  imports: [IdrPipe, DatePipe, RouterLink],
   templateUrl: './review-notif-list.component.html',
   styleUrl: './review-notif-list.component.scss'
 })
 export class ReviewNotifListComponent implements OnInit {
-  constructor(private http: HttpClient, public env: EnvironmentService, private localStorageService: LocalStorageService) {}
+  constructor(private http: HttpClient, public env: EnvironmentService, private localStorageService: LocalStorageService, public userService: UserService) {}
   ngOnInit(): void {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${this.localStorageService.getData('userToken')}`)
       this.http.get(this.env.apiUrl()+'/products/review-notif-list', {headers}).subscribe({
